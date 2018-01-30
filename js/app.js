3802c5ca1d178f2954505fdc7eee18509390c470
ulander-a <alve.ulander@chasacademy.se>
2018-01-30 18:15:40 +0100

    refactored some code, added pseudocode-placeholders

--- js/app.js
+++ js/app.js
@@ -1,49 +1,61 @@
 // Init DOM elements
-const ul = document.getElementById('photos')
+const imgList = document.getElementById("photos")
+const wordList = document.getElementById("words")
 
 // Init API Keys
 const wordsKey = "db37862dc9b2b7334362debc7f5073ef"
 const flickrKey = "dfaea809f9a59444f58aa5a4eaf4cd98"
 
-// API call functions
+// API Calls
 let thesaurus = (word) => {
-    return "http://words.bighugelabs.com/api/2/"+wordsKey+"/"+word+"/json"
+    return "http://words.bighugelabs.com/api/2/" + wordsKey + "/" + word + "/json"
 }
 
-// let flickr = ()
+let flickr = (query) => {
+    let val = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=` + flickrKey + `&text=` + query + `&per_page=33&format=json&nojsoncallback=1`
+
+    return val
+}
+
+// Methods
+function appendToList(list, data) {
+    list.innerHTML += ("<li>" + data + "</li>")
+}
 
 // Flickr fetch example
-// fetch("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=dfaea809f9a59444f58aa5a4eaf4cd98&per_page=10&format=json&nojsoncallback=1")
-// .then(response => {
-//     response.json()
-//         .then(data => {
-//             let photos = data.photos.photo
-
-//             imgUrls = []
-//             photos.forEach(element => {
-//                 img = "https://farm" + element.farm + ".staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_n.jpg"
-//                 imgUrls.push(img)
-//             })
-
-//             console.log(imgUrls)
-
-//             imgUrls.forEach(element => {
-//                 ul.innerHTML += ("<li>" + "<img src='"+element+"'</li>")
-//             })
-//         })
-// })
-// .catch(error => console.error(error))
-
+fetch(flickr("red"))
+    .then(response => {
+        response.json()
+            .then(data => {
+                console.log(data)
+            })
+    })
 
 // Thesaurus fetch example
-fetch (thesaurus("red"))
-.then(response => {
-    response.json()
-        .then(data => {
-            console.log("Nouns:")
-            console.log(data.noun.syn)
-            console.log("Adjectives:")
-            console.log(data.adjective.syn)
-        })
-})
-.catch(error => console.error(error))
+fetch(thesaurus("red"))
+    .then(response => {
+        response.json()
+            .then(data => {
+                let synoNouns = data.noun.syn
+                let synoAdjs = data.adjective.syn
+                synoNouns.forEach(element => {
+                    appendToList(wordList, element)
+                })
+
+                synoAdjs.forEach(element => {
+                    appendToList(wordList, element)
+                })
+            })
+    })
+    .catch(error => console.error(error))
+
+// trigger when form is sent
+// fetch input value as query
+function search(query) {
+    flickr(query) // fetch results from api's 
+    thesaurus(query) // fetch results from api's
+
+    // put results into array
+
+    // populate app with images and new search options from array(s)
+}
