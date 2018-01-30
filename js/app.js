f94dca2b8946deb481dd5768e7563fb3407b57e6
ulander-a <alve.ulander@chasacademy.se>
2018-01-30 16:44:05 +0100

    add more testing code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,49 @@
-// Init variables for testing
+// Init DOM elements
 const ul = document.getElementById('photos')
-const flickr = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent"
 
-fetch("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=dfaea809f9a59444f58aa5a4eaf4cd98&per_page=10&format=json&nojsoncallback=1")
+// Init API Keys
+const wordsKey = "db37862dc9b2b7334362debc7f5073ef"
+const flickrKey = "dfaea809f9a59444f58aa5a4eaf4cd98"
+
+// API call functions
+let thesaurus = (word) => {
+    return "http://words.bighugelabs.com/api/2/"+wordsKey+"/"+word+"/json"
+}
+
+// let flickr = ()
+
+// Flickr fetch example
+// fetch("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=dfaea809f9a59444f58aa5a4eaf4cd98&per_page=10&format=json&nojsoncallback=1")
+// .then(response => {
+//     response.json()
+//         .then(data => {
+//             let photos = data.photos.photo
+
+//             imgUrls = []
+//             photos.forEach(element => {
+//                 img = "https://farm" + element.farm + ".staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_n.jpg"
+//                 imgUrls.push(img)
+//             })
+
+//             console.log(imgUrls)
+
+//             imgUrls.forEach(element => {
+//                 ul.innerHTML += ("<li>" + "<img src='"+element+"'</li>")
+//             })
+//         })
+// })
+// .catch(error => console.error(error))
+
+
+// Thesaurus fetch example
+fetch (thesaurus("red"))
 .then(response => {
     response.json()
         .then(data => {
-            let photos = data.photos.photo
-
-            imgUrls = []
-            photos.forEach(element => {
-                img = "https://farm" + element.farm + ".staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_n.jpg"
-                imgUrls.push(img)
-            })
-
-            console.log(imgUrls)
-
-            imgUrls.forEach(element => {
-                ul.innerHTML += ("<li>" + "<img src='"+element+"'</li>")
-            })
+            console.log("Nouns:")
+            console.log(data.noun.syn)
+            console.log("Adjectives:")
+            console.log(data.adjective.syn)
         })
 })
-.catch(error => {
-    console.log("Something went teribly wrong! : ".error)
-})
-
-
-
-
-
+.catch(error => console.error(error))
