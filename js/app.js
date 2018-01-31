e3cccbcd1586616f2ae8c7d35b32531d08d6a459
ulander-a <alve.ulander@chasacademy.se>
2018-01-31 23:44:10 +0100

    finished basic functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 const imgList = document.getElementById("photos")
 const wordList = document.getElementById("words")
 const searchTerm = document.getElementById("term")
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+let buttons
 
 // Init API Keys
 const wordsKey = "db37862dc9b2b7334362debc7f5073ef"
@@ -17,70 +20,93 @@ let flickr = (query) => {
 }
 
 // DOM-manipulation methods
+
 let appendToList = (list, data) => {
-    list.innerHTML += "<li>" + data + "</li>"
+    if (list === wordList) {
+        list.innerHTML += "<li><button class='word-btn' value='" + data + "'>" + data + "</button></li>"
+    }
     if (list === imgList) {
         list.innerHTML += "<li><img src=" + data + " /></li>"
     }
 }
 
-// Turn flickr response to url
+function clearContent() {
+    wordList.innerHTML = ""
+    imgList.innerHTML = ""
+}
+
+let addFunctionality  = (buttons) => {
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            search(this.value)
+        })
+    });
+}
+
+function updatePage(words, imgs) {
+    clearContent()
+    words.forEach(element => {
+        appendToList(wordList, element)
+    });
+    imgs.forEach(element => {
+        appendToList(imgList, element)
+    });
+    buttons = document.querySelectorAll(".word-btn")
+    addFunctionality(buttons)
+    input.value = ""
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    search(input.value)
+})
+
+// Data handling functions
 let imgUrl = (data) => {
     return "https://farm" + data.farm + ".staticflickr.com/" + data.server + "/" + data.id + "_" + data.secret + ".jpg"
 }
 
+let handleWords = (data) => {
+    let w = []
+    data.forEach(element => {
+        element.syn.forEach(synonym => {
+            w.push(synonym)
+        })
+    })
+    return w
+}
+
 let handleData = (array) => {
     imgData = array[0].photos.photo
-    wordData = array[1].adjective.syn
 
     imgUrls = []
     imgData.forEach(element => {
         imgUrls.push(imgUrl(element))
     })
 
-    //handle words
+    wordData = []
+    if (array[1].adjective) {
+        wordData.push(array[1].adjective)
+    }
+    if (array[1].noun) {
+        wordData.push(array[1].noun)
+    }
 
-    imgUrls.forEach(element => {
-        appendToList(imgList, element)
-    });
+    words = handleWords(wordData)
 
-
-    wordData.forEach(element => {
-        appendToList(wordList, element)
-    });
+    updatePage(words, imgUrls)
 }
 
-// To do:
-// trigger when form is sent
-// fetch input value as query
 function search(query) {
-
+    searchTerm.innerText = query
     Promise.all([fetch(flickr(query)), fetch(thesaurus(query))])
         .then(res => {
-            return res.map(type => type.json() )
+            return res.map(type => type.json())
         })
         .then(res => {
             Promise.all(res)
                 .then(res => {
                     return handleData(res)
                 })
-        });
-
-    // Update "searched for:" text
-    searchTerm.innerText = query
-    // Clear input field(?)
+        })
 }
-
-search("red")
-
-                    // data.adjective.syn.forEach(word => {
-                    //     appendToList(wordList, word)
-                    // })
-
-                    // let imgUrls = []
-                    // data.photos.photo.forEach(photo => {
-                    //     imgUrls.push(imgUrl(photo))
-                    // })
-                    // imgUrls.forEach(url => {
-                    //     appendToList(imgList, url)
-                    // })
