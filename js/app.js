e1adab1d97c52d6c1ab1a56dbd7f2c68947a6320
ulander-a <alve.ulander@chasacademy.se>
2018-01-31 02:24:05 +0100

    created the barebones functionality

--- js/app.js
+++ js/app.js
@@ -1,61 +1,79 @@
 // Init DOM elements
 const imgList = document.getElementById("photos")
 const wordList = document.getElementById("words")
+const searchTerm = document.getElementById("term")
 
 // Init API Keys
 const wordsKey = "db37862dc9b2b7334362debc7f5073ef"
 const flickrKey = "dfaea809f9a59444f58aa5a4eaf4cd98"
 
 // API Calls
-let thesaurus = (word) => {
-    return "http://words.bighugelabs.com/api/2/" + wordsKey + "/" + word + "/json"
+let thesaurus = (query) => {
+    return "http://words.bighugelabs.com/api/2/" + wordsKey + "/" + query + "/json"
 }
 
 let flickr = (query) => {
-    let val = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=` + flickrKey + `&text=` + query + `&per_page=33&format=json&nojsoncallback=1`
-
-    return val
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrKey + "&text=" + query + "&per_page=33&format=json&nojsoncallback=1"
 }
 
-// Methods
-function appendToList(list, data) {
-    list.innerHTML += ("<li>" + data + "</li>")
+// DOM-manipulation methods
+let appendToList = (list, data) => {
+    list.innerHTML += "<li>" + data + "</li>"
+    if (list === imgList) {
+        list.innerHTML += "<li><img src="+data+" /></li>"
+    }
 }
 
-// Flickr fetch example
-fetch(flickr("red"))
-    .then(response => {
-        response.json()
-            .then(data => {
-                console.log(data)
-            })
-    })
+// Turn flickr response to url's
+let imgUrl = (data) => {
+    return "https://farm"+data.farm+".staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+".jpg"
+}
 
-// Thesaurus fetch example
-fetch(thesaurus("red"))
-    .then(response => {
-        response.json()
-            .then(data => {
-                let synoNouns = data.noun.syn
-                let synoAdjs = data.adjective.syn
-                synoNouns.forEach(element => {
-                    appendToList(wordList, element)
+// Flickr fetch method
+function fetchImgs(query) {
+    fetch(flickr(query))
+        .then(response => {
+            response.json()
+                .then(data => {
+                    let imgUrls = []
+                    data.photos.photo.forEach(photo => {
+                        imgUrls.push(imgUrl(photo))
+                    })
+                    imgUrls.forEach(url => {
+                        appendToList(imgList, url)
+                    })
                 })
+        })
+        .catch(error => console.error(error))
+}
 
-                synoAdjs.forEach(element => {
-                    appendToList(wordList, element)
+// Thesaurus fetch method
+function fetchWords(query) {
+    fetch(thesaurus(query))
+        .then(response => {
+            response.json()
+                .then(data => {
+                    console.log(data.adjective.syn)
+                    // Split up different word categories and append to word-list
+                    data.adjective.syn.forEach(word => {
+                        appendToList(wordList, word)
+                    });
                 })
-            })
-    })
-    .catch(error => console.error(error))
+        })
+        .catch(error => console.error(error))
 
+}
+
+// To do:
 // trigger when form is sent
 // fetch input value as query
 function search(query) {
-    flickr(query) // fetch results from api's 
-    thesaurus(query) // fetch results from api's
+    fetchWords(query)
+    fetchImgs(query)
 
-    // put results into array
-
-    // populate app with images and new search options from array(s)
+    // Update "searched for:" text
+    searchTerm.innerText = query
+    // Clear input field(?)
 }
+
+search("red")
